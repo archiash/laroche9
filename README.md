0c045d3f089bc3ef031e4e1742487303a3e5e1b1
archiash <archiash.legacy@gmail.com>
2023-12-15 14:37:17 +0700

    Finished

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# NAME:
-# STUDENT ID:
+# NAME: ไพสิฐ เลิศอนันต์พิพัฒน์
+# STUDENT ID: 660612155
--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,24 +1,41 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-//Write the function printO() here
+void printO(int row, int col)
+{
+	if (row <= 0 || col <= 0)
+	{
+		cout << "Invalid input";
+		return;
+	}
 
-int main(){
-	
-	printO(2,2);
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			cout << "O";
+		}
+		cout << endl;
+	}
+}
+
+int main()
+{
+
+	printO(2, 2);
 	cout << "\n";
-	
-	printO(3,5);
-	cout << "\n";	
-	
-	printO(5,3);
-	cout << "\n";	
-	
-	printO(0,3);
-	cout << "\n";	
-	
-	printO(7,-1);
-	cout << "\n";	
-	
+
+	printO(3, 5);
+	cout << "\n";
+
+	printO(5, 3);
+	cout << "\n";
+
+	printO(0, 3);
+	cout << "\n";
+
+	printO(7, -1);
+	cout << "\n";
+
 	return 0;
 }
